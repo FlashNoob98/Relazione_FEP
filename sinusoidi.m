34e87706614c8b987381801212f97769b7a75a10
Daniele Olivieri <danieledaniele1988@gmail.com>
2020-01-07 18:13:54 +0100

    Modificate dimensioni linee plot

--- sinusoidi.m
+++ sinusoidi.m
@@ -2,7 +2,7 @@
 %
 clear; close all; clc; %precaricamento programma
 %PARAMETRI RETE
-periodi = 2; %Definisci il numero di periodi da rappresentare
+periodi = 1; %Definisci il numero di periodi da rappresentare
 f = 50;  %Frequenza di rete
 Vrms = 220; %Valore efficace tensione stellata
 
@@ -11,6 +11,8 @@ V1n = 400; %Tensioni nominali primario e secondario
 V2n = 42;
 N = V2n/V1n; %Rapporto di trasformazione definito al contrario per comodità
 
+%PARAMETRI PLOT
+plot_width = 2;
 
 %CALCOLO
 t = linspace(0,periodi/f,1000);
@@ -21,23 +23,40 @@ V2 = Vp*sin(omega*t-2*pi/3);
 V3 = Vp*sin(omega*t-4*pi/3);
 figure();
 hold on; grid on;
+set(gca,'LineWidth',plot_width-1);
 x = t*1000; %Ai fini della rappresentazione dei tempi
-plot(x,V1,x,V2,x,V3);
+q = plot(x,V1,x,V2,x,V3);
 plot([0 x(end)],[0 0],'Color','k');
 legend('R','S','T');
 ylabel('Tensione [V]');
 xlabel('Tempo [ms]');
-%xlim([0 x(end)]);
 
-figure(); %Plot concatenate raddrizzate in uscita dal trasformatore
+for n = 1:3
+q(n).LineWidth = plot_width;
+end
+%xlim([0 x(end)]);
+%% Part 2
+%close all;
+figure(2); %Plot concatenate raddrizzate in uscita dal trasformatore
 hold on; grid on;
+set(gca,'LineWidth',plot_width-1);
 V12 = N*(V1-V2);
 V13 = N*(V1-V3);
 V23 = N*(V2-V3);
+
+V21 = N*(V2-V1);
+V31 = N*(V3-V1);
+V32 = N*(V3-V2);
 %x = x*omega/1000/pi;
-plot(x,V12,x,V23,x,V13);
+
+p = plot(x,V12,x,V13,x,V23,x,V21,x,V31,x,V32);
 %ylim([49 57]);
 ylabel('Tensione [V]');
 xlabel('Tempo [ms]');
+plot([0 x(end)],[0 0],'Color','k');
+legend('V_{12}','V_{13}','V_{23}','V_{21}','V_{31}','V_{32}');
 
-
+%Costrutto per modificare lo spessore delle linee
+for n = 1:6
+p(n).LineWidth = plot_width;
+end
